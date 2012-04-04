5793758551500e711f3dfedf6d7bd8d1bcd3b849
Mike Li <mike0804@MikeLi-Server.(none)>
2012-04-03 20:19:52 -0700

    Remove test code from class file

--- html_element.php
+++ html_element.php
@@ -196,26 +196,4 @@ class html_element
    
 }
 
-$div = new html_element('div');
-$div->add_class('borderless');
-$div->add_class('seemless');
-$div->add_class('microphone');
-$div->remove_class('microphone');
-$div->set('id'     , 'main');
-$div->set('fdata'  , '{"1":"77"}');
-$div->set('title'  , 'astitle');
-$div->remove('title'  , 'astitle');
-$div->css('display', 'none');
-$div->css('border' , '1px solid black');
-$div->css('height' , '80px');
-$div->css('height' , '');
-
-$a = new html_element('a');
-$a->set('href', 'http://www.google.com');
-$a->text('google');
-
-$div->append('I always like to use ');
-$div->append($a);
-$div->append(', how about you?');
-$div->output();
 ?>
